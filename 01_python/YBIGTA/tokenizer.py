cd9bc2d24aa63a7765134eae938377cf43a75c5c
danielredglasses <sk091204@gmail.com>
2024-01-16 23:01:59 +0900

    Update tokenizer.py and tokenizers.py

--- 01_python/YBIGTA/tokenizer.py
+++ 01_python/YBIGTA/tokenizer.py
@@ -2,51 +2,74 @@ from typing import Optional, Union, List
 
 class Tokenizer:
     def __init__(self, corpus: Optional[Union[List[str], str]] = None):
-        # Initialize self.corpus
-        self.corpus = {}
+        '''
+        Args:
+            corpus (List[str], str): preprocessed string
+        Returns:
+            None
+        '''
+        self.corpus = []
+        self.word_freq = {}
         self.add_corpus(corpus)
 
-    def add_corpus(self, corpus: Optional[Union[List[str], str]] = None) -> None:
+    def add_corpus(self, 
+                   corpus: Optional[Union[List[str], str]] = None
+    ) -> None:
+        '''
+        Add corpus to self.corpus and update word_freq
+        Args:
+            corpus (List[str], str): preprocessed string
+        Returns:
+            None
+        '''
         if isinstance(corpus, str):
-            if corpus in self.corpus:
-                self.corpus[corpus] += 1
-            else:
-                self.corpus[corpus] = 1
-        elif isinstance(corpus, list):
-            for word in corpus:
-                if word in self.corpus:
-                    self.corpus[word] += 1
+            # Update self.corpus
+            self.corpus.append(corpus)
+
+            # Update self.word_freq
+            words = self.text_to_words(corpus)
+            for word in words:
+                if word in self.word_freq:
+                    self.word_freq[word] += 1
                 else:
-                    self.corpus[word] = 1
+                    self.word_freq[word] = 1
+        elif isinstance(corpus, list):
+            # Update self.corpus
+            self.corpus += corpus
+
+            # Update self.word_freq
+            for text in corpus:
+                words = self.text_to_words(text)
+                for word in words:
+                    if word in self.word_freq:
+                        self.word_freq[word] += 1
+                    else:
+                        self.word_freq[word] = 1
         else:
             raise TypeError('corpus is neither a string nor a list of strings')
-        
-    def get_corpus(self):
-        return self.corpus
-    
-    def train(self, n_iter: int) -> None:
-        pass
 
+    @staticmethod
     def tokenize(self,
                  text: Union[List[str], str],
                  padding: bool = False,
                  max_length: Optional[int] = None
     ) -> Union[List[List[int]], List[int]]:
+        '''
+        Tokenize the input text into a list of token IDs
+        Args:
+            text (List[str], str): preprocessed text
+            padding (bool): whether or not to add padding token
+            max_length (int): maximum length of list of tokens
+        Return:
+            List of token IDs from input text
+        '''
         # Need to convert text to token and truncate if necessary
         text_tokens = []
         if isinstance(text, str):
-            word_tokens = self.text_to_tokens(text)
-            if len(word_tokens) <= max_length:
-                text_tokens.append(word_tokens)
-            else:
-                text_tokens.append(word_tokens[:max_length])
-        elif isinstance(text, str):
-            for word in text:
-                word_tokens = self.text_to_tokens(word)
-                if len(word_tokens) <= max_length:
-                    text_tokens.append(word_tokens)
-                else:
-                    text_tokens.append(word_tokens[:max_length])
+            text_tokens.append(self.text_to_tokens(text, max_length))
+        elif isinstance(text, list):
+            for curtext in text:
+                text_tokens.append(self.text_to_tokens(curtext, max_length))
         else:
             raise TypeError('text is neither a string nor a list of strings')
 
@@ -57,7 +80,7 @@ class Tokenizer:
 
         # Need to add padding token if necessary
         if padding:
-            token_padding = 1
+            token_padding = self.tokens.index('*')
             max_length_word_tokens = 0
             for word_tokens in text_tokens:
                 max_length_word_tokens = max(max_length_word_tokens, len(word_tokens))
@@ -67,18 +90,64 @@ class Tokenizer:
         
         return text_tokenIDs
 
-    def text_to_tokens(self, word):
-        word_tokens = []
-        cur_token = ''
-        for i in range(len(word)):
-            if i+1 == len(word) or str(cur_token + word[i]) not in self.tokens:
-                word_tokens.append(cur_token)
-                cur_token = ''
-            else:
-                cur_token += word[i]
-        return word_tokens
+    @staticmethod
+    def text_to_words(self, 
+                      text: str
+    ) -> List[str]:
+        '''
+        Split the text into words based on whitespace and punctuation (not necessary rule)
+        Args:
+            text (str): preprocessed text
+        Return:
+            List of words splitted from text
+        '''
+        return text.split(' ')
+
+    @staticmethod
+    def text_to_tokens(self, 
+                       text: str, 
+                       max_length: Optional[int] = None
+    ) -> List[str]:
+        '''
+        Convert from text to list of tokens
+        Args:
+            text (str): unpreprocessed text
+            max_length (int): maximum length of list of tokens
+        Returns:
+            List of tokens from text
+        '''
+        # Convert from text to words
+        words = self.text_to_words(text)
+
+        # Split words into tokens
+        text_tokens = []
+        for word in words:
+            cur_token = ''
+            for i in range(len(word)):
+                if str(cur_token + word[i]) not in self.tokens:
+                    text_tokens.append(cur_token)
+                    cur_token = word[i]
+                else:
+                    cur_token += word[i]
+            text_tokens.append(cur_token)
+
+        # Truncate text_tokens if its length is greater than max_length
+        if max_length is not None and len(text_tokens) > max_length:
+            text_tokens = text_tokens[:max_length]
+
+        return text_tokens
     
-    def tokens_to_tokenIDs(self, word_tokens):
+    @staticmethod
+    def tokens_to_tokenIDs(self, 
+                           word_tokens: List[str]
+    ) -> List[int]:
+        '''
+        Convert from tokens to their corresponding token IDs
+        Args:
+            word_tokens (List[str]): a list of tokens
+        Returns:
+            List of their corresponding token IDs
+        '''
         word_tokenIDs = []
         for i in range(len(word_tokens)):
             word_tokenIDs.append(self.tokens.index(word_tokens[i]))
--- 01_python/YBIGTA/tokenizers.py
+++ 01_python/YBIGTA/tokenizers.py
@@ -1,14 +1,32 @@
 from typing import Optional, Union, List
-from tokenizer import Tokenizer
+from YBIGTA.tokenizer import Tokenizer
 
 class BPETokenizer(Tokenizer):
-    def __init__(self, corpus: Optional[Union[List[str], str]] = None):
+    def __init__(self, 
+                 corpus: Optional[Union[List[str], str]] = None):
+        '''
+        Args:
+            corpus (List[str], str): preprocessed string
+        Returns:
+            None
+        '''
         super().__init__(corpus)
 
-    def train(self, n_iter: int) -> None:
+    def train(self, 
+              n_iter: int
+    ) -> None:
+        '''
+        Perform merge operation n_iter times in order to find
+        a list of tokens
+        Args:
+            n_iter (int): number of iterations
+        Returns:
+            None
+        '''
         if not isinstance(n_iter, int) or n_iter < 1:
             raise TypeError('number of iterations (n_iter) needs to be a positive integer')
         
+        self.tokens = []
         for _ in range(n_iter):
             self.merge()
 
